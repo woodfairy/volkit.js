b5fe272bf91cac6030cd8b2bad41e3dd3c53e5bd
Dan Schmit <d.schmit@m3connect.de>
2022-02-13 14:12:52 +0100

    fix IIFE

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const volkit = (function () {
-    const getRandomString = length => {
-        const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@:;+-_ß';
-        let arr = new Uint16Array(length);
-        crypto.getRandomValues(arr);
-        arr = arr.map(x => validChars.charCodeAt(x % validChars.length));
-        return String.fromCharCode.apply(null, arr);
-    }
-  
-    // Return what others can use
-    return {
-      getRandomString: getRandomString
-    }
-  })()
+const volkit = VOLCORE_BASE_URL => {
+  const getRandomString = length => {
+      const validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@:;+-_ß';
+      let arr = new Uint16Array(length);
+      crypto.getRandomValues(arr);
+      arr = arr.map(x => validChars.charCodeAt(x % validChars.length));
+      return String.fromCharCode.apply(null, arr);
+  }
 
-exports.volkit = volkit
+  // Return what others can use
+  return {
+    getRandomString: getRandomString
+  }
+}
+
+exports.init = volkit
